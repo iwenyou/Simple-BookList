5d72a80b3d39ba4284e5640566195ab2fab7be02
Wen Zhang <iwenyou@gmail.com>
2017-07-26 22:58:46 -0700

    reducer added

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return[
+    {title: 'Javascrpt: the good part'},
+    {title: 'Harry potter'},
+    {title: 'The Dark Tower'},
+    {title: 'The Three Body Problem'}
+  ]
+}
