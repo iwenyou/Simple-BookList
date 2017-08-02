8ee3d98cb6e6da35ed23ce79fe0f5dac1d22c453
Wen Zhang <iwenyou@gmail.com>
2017-08-01 22:37:35 -0700

    book list linked to book detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,6 @@ import ActiveBook from './reducer_active_book';
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBook
-
 });
 
 export default rootReducer;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return[
-    {title: 'Javascrpt: the good part'},
-    {title: 'Harry potter'},
-    {title: 'The Dark Tower'},
-    {title: 'The Three Body Problem'}
+    {title: 'Javascrpt: the good part',pages: 101},
+    {title: 'Harry potter', pages:200},
+    {title: 'The Dark Tower', pages:300},
+    {title: 'The Three Body Problem', pages:400}
   ];
 }
